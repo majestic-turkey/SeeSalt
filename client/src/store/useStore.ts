d9dad434762dc7761f96d2d84de7e95087e0cabf
A Majestic Turkey <sodium.smith@gmail.com>
2026-05-04 01:09:49 -0400

    Build out the store to hold the socket instance and user info, and update main.tsx to use BrowserRouter from react-router-dom instead of react-router.

--- client/src/store/useStore.ts
+++ client/src/store/useStore.ts
@@ -1,14 +1,31 @@
 import { create } from 'zustand';
-import type { ClientToServerEvents, ServerToClientEvents } from '../types';
+import type { ClientToServerEvents, ServerToClientEvents, User } from '../types';
 import { Socket } from 'socket.io-client';
 
+interface StoreState {
+    socket: Socket<ServerToClientEvents, ClientToServerEvents> | null;
+    setSocket: (socket: Socket<ServerToClientEvents, ClientToServerEvents>) => void;
+
+    username: string;
+    setUsername: (username: string) => void;
+
+    roomId: string | null;
+    setRoomId: (roomId: string) => void;
+
+    users: User[];
+    setUsers: (users: User[]) => void;
+}
+
 // Create a Zustand store to hold the socket instance
-const useSocketStore = create<{
-  socket: Socket<ServerToClientEvents, ClientToServerEvents> | null;
-  setSocket: (socket: Socket<ServerToClientEvents, ClientToServerEvents>) => void;
-}>((set) => ({
-  socket: null,
-  setSocket: (socket) => set({ socket }),
+const useStore = create<StoreState>((set) => ({
+    socket: null,
+    setSocket: (socket) => set({ socket }),
+    username: '',
+    setUsername: (username) => set({ username }),
+    roomId: null,
+    setRoomId: (roomId) => set({ roomId }),
+    users: [],
+    setUsers: (users) => set({ users }),
 }));
 
-export default useSocketStore;
+export default useStore;
